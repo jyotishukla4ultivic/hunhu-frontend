6073f3f7de4842f191ec5c2c7e3652f6095bba66
jyotishukla4ultivic <jyoti.shukla@ultivic.com>
2025-06-27 13:24:54 +0530

    fixes

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+// No polyfills needed for this project.
+
+/***************************************************************************************************
+ * Load `$localize` if used for i18n support. If you do not use Angular i18n, you can remove this.
+ */
+// import '@angular/localize/init';
+
+// Add any additional polyfills here if needed in the future. 
