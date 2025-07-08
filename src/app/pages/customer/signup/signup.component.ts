0ec907c3a422bfb11ea907c0e054b782dceb0d46
jyotishukla4ultivic <jyoti.shukla@ultivic.com>
2025-07-08 20:01:58 +0530

    customer  module integration

---
+++ src/app/pages/customer/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class CustomerSignupComponent {
+  // Add properties and methods for signup logic here
+  constructor() {}
+} 
